411c574334afa450f4bac356ef7647010cee4421
Muqaddam <muhammadmustufa555@gmail.com>
2022-12-19 12:31:13 +0500

    changes

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -1,77 +1,79 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\Appointments;
 use App\Models\TimeSlots;
+
 class AppointmentController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
         $date = date('y-d-m');
-        $slots = TimeSlots::where('date',$date )->get();
+        $slots = TimeSlots::where('date', $date)->get();
         return view("appointment.index", compact("slots"));
     }
-    public function detail(Request $req){
+    public function detail(Request $req)
+    {
         $req->validate([
             'appointment_date' => 'required',
             'appointment_time' => 'required',
             'appointment_discuss' => 'required',
         ]);
-     
+
         Appointments::create([
-            'appointment_date'=>date("Y-m-d", strtotime($req->appointment_date)),
-            'appointment_time'=> $req->appointment_time,
-            'appointment_discuss'=> $req->appointment_discuss,
+            'appointment_date' => date("Y-m-d", strtotime($req->appointment_date)),
+            'appointment_time' => $req->appointment_time,
+            'appointment_discuss' => $req->appointment_discuss,
         ]);
         TimeSlots::create([
-            'date'=>date("Y-m-d", strtotime($req->appointment_date)),
-            'time'=>date("h:i", strtotime($req->appointment_time)),
-            'booked'=>1,
+            'date' => date("Y-m-d", strtotime($req->appointment_date)),
+            'time' => date("h:i", strtotime($req->appointment_time)),
+            'booked' => 1,
         ]);
         return view("registerBussiness");
     }
 
-    public function slots($date,$month,$year){
-        
+    public function slots($date, $month, $year)
+    {
+
         date_default_timezone_set('Asia/Karachi');
 
         $script_tz = date_default_timezone_get();
-        $whole_date = $year.'-'.$date.'-'.$month;
-        
+        $whole_date = $year . '-' . $date . '-' . $month;
+
 
         $time = date("H:i:s");
         $now_date  = date('Y-m-d');
-       
-        if($now_date == $whole_date){
-            $slots = TimeSlots::where('date',$year.'-'.$date.'-'.$month)->where("time",">",$time )->where('booked',0)->get();
+
+        if ($now_date == $whole_date) {
+            $slots = TimeSlots::where('date', $year . '-' . $date . '-' . $month)->where("time", ">", $time)->where('booked', 0)->get();
+        } else {
+            $slots = TimeSlots::where('date', $year . '-' . $date . '-' . $month)->where('booked', 0)->get();
         }
-        else{
-        $slots = TimeSlots::where('date',$year.'-'.$date.'-'.$month)->where('booked',0)->get();
-        }
-       // $slots = TimeSlots::where('date',$year.'-'.$date.'-'.$month)->where('booked',0)->get();
-        
+        // $slots = TimeSlots::where('date',$year.'-'.$date.'-'.$month)->where('booked',0)->get();
+
         //$slots = TimeSlots::where('date',"2023-03-17")->get();
-        $art = ""; 
-        $n= 1;
-       foreach($slots as $slot){
-         $slot = $slot->time;
-         $art .= '<div class="form-check" ><input class="form-check-input" type="radio" name="appointment_time" id="tag';
-         $art .= $n;
-         $art .= '" value="9:00 to 8:00">
+        $art = "";
+        $n = 1;
+        foreach ($slots as $slot) {
+            $slot = $slot->time;
+            $art .= '<div class="form-check" ><input class="form-check-input" type="radio" name="appointment_time" id="tag';
+            $art .= $n;
+            $art .= '" value="9:00 to 8:00">
          <label class="form-check-label" for="tag';
-         $art .= $n;
-         $art .= '">';
-         
-          $art .= date('h:i a', strtotime($slot ));
-       
-         $art .='</label></div>';
-         $n = $n+1;
-       }
-        
+            $art .= $n;
+            $art .= '">';
+
+            $art .= date('h:i a', strtotime($slot));
+
+            $art .= '</label></div>';
+            $n = $n + 1;
+        }
+
         return array('slots' => $art);
-    
-       
     }
 }
