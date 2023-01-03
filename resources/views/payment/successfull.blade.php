172e9fb8d13268d64f5f69e6fd6de7cf74c32e0f
Muqaddam <muhammadmustufa555@gmail.com>
2023-01-03 12:19:24 +0500

    changes

--- resources/views/payment/successfull.blade.php
+++ resources/views/payment/successfull.blade.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-@include("layouts.head")
+@include('layouts.head')
+
 <body>
     <div class="main">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
-                    
-                        
-                   
-                    @if($payments["pp_ResponseCode"]== "000")
-                    <div class="card-header">Payment Successful</div>
-                    @else
-                    <div class="card-header">Payment Unsuccessfull</div>
-                    @endif
+
+
+
+                        @if ($payments['pp_ResponseCode'] == '000')
+                            <div class="card-header">Payment Successful</div>
+                        @else
+                            <div class="card-header">Payment Unsuccessfull</div>
+                        @endif
                         <div class="card-body">
-                            <h1>{{$payments["pp_ResponseMessage"]}}</h1>
+                            <h1>{{ $payments['pp_ResponseMessage'] }}</h1>
                             <h4>Payment Information</h4>
-                            <p>Transaction Id :{{$payments["pp_TxnRefNo"]}} </p>
-                  
-                   
+                            <p>Transaction Id :{{ $payments['pp_TxnRefNo'] }} </p>
+
+
                         </div>
-                   
+
                     </div>
                 </div>
             </div>
