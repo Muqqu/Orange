61baa24f83761831ad0972ffff4ce7f7d98b5e42
Muqaddam <muhammadmustufa555@gmail.com>
2023-01-14 12:23:32 +0500

    changes

--- resources/views/easypaisa.blade.php
+++ resources/views/easypaisa.blade.php
@@ -1,11 +1,12 @@
 @php
     $easypaisa_environment = config('easypaisa.environment');
-
+    
 @endphp
 
-<form name="redirect-to-payment-gateway" method="POST" action="{{Config::get('constants.easypay.TRANSACTION_POST_URL1') }}">
-   @foreach($data as $key => $value)
+<form name="redirect-to-payment-gateway" method="POST"
+    action="{{ Config::get('constants.easypay.TRANSACTION_POST_URL1') }}">
+    @foreach ($data as $key => $value)
         <input type="hidden" name="{{ $key }}" value="{{ $value }}">
     @endforeach
 </form>
-@include("layouts.footer");
+@include('layouts.footer');
