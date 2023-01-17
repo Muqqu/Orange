2edfda06add901bf35cdb8f02251e2d70559662a
Muqaddam <muhammadmustufa555@gmail.com>
2023-01-17 12:25:30 +0500

    changes

--- resources/views/jazzCash.blade.php
+++ resources/views/jazzCash.blade.php
@@ -2,8 +2,8 @@
     $jazzcash_environment = config('jazzcash.environment');
 @endphp
 <form name="redirect-to-payment-gateway" method="POST" action="{{ config("jazzcash.$jazzcash_environment.endpoint") }}">
-   @foreach($data as $key => $value)
+    @foreach ($data as $key => $value)
         <input type="hidden" name="{{ $key }}" value="{{ $value }}">
     @endforeach
 </form>
-@include("layouts.footer");
+@include('layouts.footer');
