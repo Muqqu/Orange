a51288c39a27fb88c71c1597694cfa596b5b190d
Muqaddam <muhammadmustufa555@gmail.com>
2023-03-09 12:04:56 +0500

    changes

--- resources/views/appointment/detail.blade.php
+++ resources/views/appointment/detail.blade.php
@@ -2,107 +2,114 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="assets/css/jquery-ui.min.css" />
-  <link rel="stylesheet" href="assets/css/slick.css" />
-  <link rel="stylesheet" href="assets/css/slick-theme.css" />
-  <link rel="stylesheet" href="assets/fonts/fontawesome-free-5.15.4-web/css/all.css" />
-  <link rel="stylesheet" href="assets/scss/style.css" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css" />
+    <link rel="stylesheet" href="assets/css/slick.css" />
+    <link rel="stylesheet" href="assets/css/slick-theme.css" />
+    <link rel="stylesheet" href="assets/fonts/fontawesome-free-5.15.4-web/css/all.css" />
+    <link rel="stylesheet" href="assets/scss/style.css" />
 
 
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&family=Darker+Grotesque:wght@600&display=swap"
-    rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&family=Darker+Grotesque:wght@600&display=swap"
+        rel="stylesheet" />
 
-  <title>coding</title>
+    <title>coding</title>
 </head>
 
 <body>
-  <div class="main">
-    <header class="header">
-      <nav class="navbar">
-        <div class="container-fluid">
-          <a href="" class="navbar-brand"><img src="assets/img/svg/logo.svg" alt="" /></a>
-          <div class="d-flex justify-content-end">
-            <ul class="social d-none">
-              <li class="list-item"><a href=""><i class="fab fa-facebook-f"></i></a></li>
-              <li class="list-item"><a href=""><i class="fab fa-instagram"></i></a></li>
-              <li class="list-item"><a href=""><i class="fab fa-twitter"></i></a></li>
-              <li class="list-item"><a href=""><i class="fab fa-discord"></i></a></li>
-            </ul>
-            <div class="btn btn-primary">Book Aappointment</div>
-          </div>
+    <div class="main">
+        <header class="header">
+            <nav class="navbar">
+                <div class="container-fluid">
+                    <a href="" class="navbar-brand"><img src="assets/img/svg/logo.svg" alt="" /></a>
+                    <div class="d-flex justify-content-end">
+                        <ul class="social d-none">
+                            <li class="list-item"><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                            <li class="list-item"><a href=""><i class="fab fa-instagram"></i></a></li>
+                            <li class="list-item"><a href=""><i class="fab fa-twitter"></i></a></li>
+                            <li class="list-item"><a href=""><i class="fab fa-discord"></i></a></li>
+                        </ul>
+                        <div class="btn btn-primary">Book Aappointment</div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="main-content">
+            <section class="appointment">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="map-wrapper mb-4 mb-lg-0">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.6173560727525!2d-73.98785308434377!3d40.74844454332149!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2s!4v1678699378141!5m2!1sen!2s"
+                                    width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <h1 class="title">You can find us here.</h1>
+                            <a href="#" class="text mb-3"><i class="fas fa-map-marker-alt me-3 text-primary"></i>
+                                Empire State Building, Empire State Building, West 34th Street, New York, NY, USA</a>
+                            <a href="tel:" class="text mb-3"><i class="fas fa-phone me-3 text-primary"></i>+07 8927
+                                789</a>
+                            <a href="mailto:info@orange.com" class="text"><i
+                                    class="fas fa-envelope me-3 text-primary"></i>info@orange.com</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-      </nav>
-    </header>
-    <div class="main-content">
-      <section class="appointment">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-6">
-              <div class="map-wrapper mb-4 mb-lg-0">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.6173560727525!2d-73.98785308434377!3d40.74844454332149!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2s!4v1678699378141!5m2!1sen!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-              </div>
+
+        <div class="footer d-none">
+            <h1 class="footer-title">Orange</h1>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p class="text text-dark ms-lg-4 mb-0 d-flex gap-2">Got a question?<a href="#"
+                                class="text-dark">Contact
+                                us</a></p>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="d-flex gap-5 align-items-center justify-content-md-end me-lg-4">
+                            <p class="text text-dark m-0">© 2023 Orange</p>
+                            <ul class="d-flex gap-4 gap-lg-5 p-0 m-0">
+                                <li>
+                                    <a href="" class="text text-dark m-0">Privacy Policy</a>
+                                </li>
+                                <li>
+                                    <a href="" class="text text-dark m-0">About us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-            <div class="col-lg-6">
-              <h1 class="title">You can find us here.</h1>
-              <a href="#" class="text mb-3"><i class="fas fa-map-marker-alt me-3 text-primary"></i> Empire State Building, Empire State Building, West 34th Street, New York, NY, USA</a>
-              <a href="tel:" class="text mb-3"><i class="fas fa-phone me-3 text-primary"></i>+07 8927 789</a>
-              <a href="mailto:info@orange.com" class="text"><i class="fas fa-envelope me-3 text-primary"></i>info@orange.com</a>
-            </div>
-          </div>
         </div>
-      </section>
+
+
+        <div class="cursor"></div>
     </div>
 
-    <div class="footer d-none">
-      <h1 class="footer-title">Orange</h1>
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-6">
-            <p class="text text-dark ms-lg-4 mb-0 d-flex gap-2">Got a question?<a href="#" class="text-dark">Contact
-                us</a></p>
-          </div>
-          <div class="col-md-6">
-            <div class="d-flex gap-5 align-items-center justify-content-md-end me-lg-4">
-              <p class="text text-dark m-0">© 2023 Orange</p>
-              <ul class="d-flex gap-4 gap-lg-5 p-0 m-0">
-                <li>
-                  <a href="" class="text text-dark m-0">Privacy Policy</a>
-                </li>
-                <li>
-                  <a href="" class="text text-dark m-0">About us</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+    <script src="assets/js/jquery-3.6.3.min.js"></script>
+    <script src="assets/js/jquery-ui.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-      </div>
-    </div>
-
-
-    <div class="cursor"></div>
-  </div>
-
-  <script src="assets/js/jquery-3.6.3.min.js"></script>
-  <script src="assets/js/jquery-ui.min.js"></script>
-  <script src="assets/js/bootstrap.bundle.min.js"></script>
-
-  <script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.14.2/TweenMax.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/1.3.0/jquery.scrollmagic.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/1.3.0/jquery.scrollmagic.debug.js"></script>
-  <!-- <script src="assets/js/cursor.js"></script> -->
-  <!-- <script src="assets/js/window.curDot.min.js"></script> -->
-  <script src="assets/js/slick.min.js"></script>
-  <script src="assets/js/particles.js"></script>
-  <script src="assets/js/aos.js"></script>
-  <script src="assets/js/custom.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.14.2/TweenMax.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/1.3.0/jquery.scrollmagic.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/1.3.0/jquery.scrollmagic.debug.js"></script>
+    <!-- <script src="assets/js/cursor.js"></script> -->
+    <!-- <script src="assets/js/window.curDot.min.js"></script> -->
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/particles.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/custom.js"></script>
 </body>
 
 </html>
