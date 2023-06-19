33971782242a071a3626ffcf846ee3713cc07bc4
Muqaddam <muhammadmustufa555@gmail.com>
2023-06-19 12:37:42 +0500

    changes

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Ideas;
+
 class IdeaController extends Controller
 {
     //
     public function index()
     {
         # code...
-        
+
         return view("joinIdea");
     }
     public function register(Request $request)
@@ -18,18 +19,18 @@ class IdeaController extends Controller
         # code...
         $request->validate([
             'title' => 'required',
-            'motive'=>'required',
-            'message'=> 'required',
+            'motive' => 'required',
+            'message' => 'required',
             'email' => 'required',
-            'contact_no'=> 'required'
+            'contact_no' => 'required'
         ]);
 
         Ideas::create([
             'title' => $request->title,
-            'message'=> $request->message,
-            'motive'=> $request->motive,
-            'email'=> $request->email,
-            'contact_no'=> $request->contact_no
+            'message' => $request->message,
+            'motive' => $request->motive,
+            'email' => $request->email,
+            'contact_no' => $request->contact_no
         ]);
         return redirect()->back();
     }
