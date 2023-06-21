a4c10f0d9d2076716ecdc23a2cf5c751d9e99ca4
Muqaddam <muhammadmustufa555@gmail.com>
2023-06-21 12:38:39 +0500

    changes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,74 +13,75 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
         return view('auth.index');
     }
 
     public function customLogin(Request $request)
     {
-   
+
         $request->validate([
             'email' => 'required',
             'password' => 'required',
         ]);
-   
+
         $credentials = $request->only('email', 'password');
-      
+
         if (Auth::attempt($credentials)) {
-            $user=Auth::User();
-            if($user->role_id == "Appointment"){
-                 return view("appointment.detail");
-            }
-            else{
-            return redirect()->route('dashboard')->with([
-                'message' => 'Your logged in successfully',
-                'alert-type' => 'success',
-                'username' => $user->name,
-            ]);
+            $user = Auth::User();
+            if ($user->role_id == "Appointment") {
+                return view("appointment.detail");
+            } else {
+                return redirect()->route('dashboard')->with([
+                    'message' => 'Your logged in successfully',
+                    'alert-type' => 'success',
+                    'username' => $user->name,
+                ]);
             }
         }
-  
+
         return redirect("/login")->withSuccess('Login details are not valid');
     }
     public function dashboard()
     {
         $videos = Videos::all();
-   
-        if(Auth::check()){
-            return view('dashboard.index',compact("videos"));
+
+        if (Auth::check()) {
+            return view('dashboard.index', compact("videos"));
         }
-  
+
         return redirect("/login")->withSuccess('You are not allowed to access');
     }
 
     public function customRegistration(Request $request)
-    {  
+    {
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8',
             'cpassword' => 'required|min:8|same:password',
         ]);
-           
+
         $data = $request->all();
         $check = $this->create($data);
-         
+
         return redirect("dashboard")->withSuccess('You have signed-in');
     }
 
     public function create(array $data)
     {
-      return User::create([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'password' => Hash::make($data['password'])
-      ]);
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
     }
-    public function signOut() {
+    public function signOut()
+    {
         Session::flush();
         Auth::logout();
-  
+
         return Redirect('login');
     }
 
@@ -88,34 +89,24 @@ class LoginController extends Controller
     {
         $videos = Videos::find($id);
         return array('video' => $videos);
-
-   
     }
 
     public function forgotPassword()
     {
         return view('auth.forgotpassword');
-
-   
     }
     public function resetPassword($email)
     {
-        return view('auth.resetpassword',compact("email"));
-
-   
+        return view('auth.resetpassword', compact("email"));
     }
     public function reset(Request $request)
     {
-        
-        $user = User::where("email",$request->email.".com");
+
+        $user = User::where("email", $request->email . ".com");
         $user->update([
             'password' => Hash::make($request->password)
         ]);
         $videos = Videos::all();
-        return view('dashboard.index',compact("videos"));
-
-   
+        return view('dashboard.index', compact("videos"));
     }
-
-
 }
